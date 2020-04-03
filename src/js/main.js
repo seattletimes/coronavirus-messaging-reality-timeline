898a1e6e494cc3b844edc3a8801a73fd8335d45a
emilymeng <eeng@seattletimes.com>
2020-04-03 15:09:58 -0700

    init

---
+++ src/js/main.js
@@ -0,0 +1,20 @@
+// var paywall = require("./lib/paywall");
+// setTimeout(() => paywall(12345678), 5000);
+
+require("component-responsive-frame/child");
+
+var $ = require("./lib/qsa");
+// require("./audio");
+
+var uncollapseButton = $.one("#uncollapseButton");
+var uncollapse = function uncollapse() {
+  $.one(".shootings").classList.remove("collapsed");
+};
+uncollapseButton.addEventListener("click", uncollapse);
+uncollapseButton.addEventListener("keydown", (ev) => {
+  // enter or spacebar, mimicking <button>'s behavior
+  if (ev.keyCode === 13 || ev.keyCode === 32){
+    ev.preventDefault(); // avoid, e.g., scroll down on spacebar
+    uncollapse();
+  }
+});
